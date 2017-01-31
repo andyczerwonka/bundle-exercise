fb8740ec98f207e9e5ce332bb6e76d336ae4172c
Andy Czerwonka <andy.czerwonka@gmail.com>
2017-01-31 06:00:22 -0700

    #1 present the bundles with the most discount so that the bundler find the cheapest option plus eliminate bundles that are more expensive than retail

--- src/main/scala/bundler/Bundler.scala
+++ src/main/scala/bundler/Bundler.scala
@@ -9,34 +9,38 @@ class Cart {
   private val _items: ListBuffer[Item] = ListBuffer.empty
   def add(item: Item*): Unit = _items ++= item
   def remove(item: Item*): Unit = _items --= item
-  def contents(): List[Item] = _items.toList
+  def contents(): Seq[Item] = _items.toList
 }
 
-case class Bundle(items: Seq[Item], price: Double)
+case class Bundle(items: Seq[Item], price: Double) {
+  val discount: Double = items.foldLeft(0.0)(_ + _.price) - price
+}
 
-class Bundler(all: Set[Bundle], cart: Cart) {
+class Bundler(available: Seq[Bundle], cart: Cart) {
 
-  case class Bundled(bundles: Seq[Bundle], remaining: Seq[Item]) {
+  case class BundledItems(bundles: Seq[Bundle], remaining: Seq[Item]) {
     lazy val total: Double = {
       val bundledTotal = bundles.foldLeft(0.0)(_ + _.price)
       val remainderTotal = remaining.foldLeft(0.0)(_ + _.price)
       bundledTotal + remainderTotal
     }
-    lazy val discount: Double = cart.contents().foldLeft(0.0)(_ + _.price) - total
+    lazy val discount: Double = bundles.foldLeft(0.0)(_ + _.discount)
   }
 
-  lazy val contents: Bundled = {
-    def min(b1: Bundled, b2: Bundled): Bundled = if (b1.total < b2.total) b1 else b2
+  lazy val contents: BundledItems = {
     val cartItems = cart.contents()
-    val applicableBundles = all.filter(b => cartItems.intersect(b.items).length == b.items.length)
-    bundle(applicableBundles, Nil, cartItems)
+    val applicableBundles = available
+      .filter(_.discount > 0)
+      .filter(b => cartItems.intersect(b.items).length == b.items.length)
+    val bestDealFirst = applicableBundles.sortWith(_.discount > _.discount)
+    bundle(bestDealFirst, Nil, cartItems)
   }
 
   @tailrec
-  private def bundle(all: Set[Bundle], accumulator: List[Bundle], remaining: Seq[Item]): Bundled = {
+  private def bundle(all: Seq[Bundle], accumulator: List[Bundle], remaining: Seq[Item]): BundledItems = {
     all.find(found => remaining.intersect(found.items).length == found.items.length) match {
       case Some(b) => bundle(all, b :: accumulator, remaining.diff(b.items))
-      case None => Bundled(accumulator, remaining)
+      case None => BundledItems(accumulator, remaining)
     }
   }
 
--- src/test/scala/bundler/BundlerSpec.scala
+++ src/test/scala/bundler/BundlerSpec.scala
@@ -10,10 +10,10 @@ class BundlerSpec extends FlatSpec with Matchers {
   val bread = Item("Bread", 3.0)
   val butter = Item("Butter", 3.0)
 
-  val twoApplesBundle = Set(Bundle(List(apple, apple), 2.19))
-  val freeButterBundle = Set(Bundle(List(bread, butter, butter), 6.0))
+  val twoApplesBundle = Seq(Bundle(List(apple, apple), 2.19))
+  val freeButterBundle = Seq(Bundle(List(bread, butter, butter), 6.0))
 
-  val bothBundles = twoApplesBundle ++ freeButterBundle
+  val bothBundles: Seq[Bundle] = twoApplesBundle ++ freeButterBundle
 
   "The Bundler" should "charge $1.99 for one apple" in {
     val cart = new Cart()
@@ -44,7 +44,7 @@ class BundlerSpec extends FlatSpec with Matchers {
   it should "charge $3.98 for two apples without the apples bundle" in {
     val cart = new Cart()
     cart.add(apple, apple)
-    val bundler = new Bundler(Set.empty, cart)
+    val bundler = new Bundler(Nil, cart)
     bundler.contents.total should be (3.98)
     bundler.contents.discount should be (0.0)
     bundler.contents.bundles shouldBe empty
@@ -75,7 +75,7 @@ class BundlerSpec extends FlatSpec with Matchers {
     val bundler = new Bundler(bothBundles, cart)
     bundler.contents.total should be (11.19)
     bundler.contents.discount should be (4.79 +- epsilon)
-    bundler.contents.remaining should contain only (butter)
+    bundler.contents.remaining should contain only butter
   }
 
   it should "charge $0 for an empty cart" in {
@@ -87,17 +87,29 @@ class BundlerSpec extends FlatSpec with Matchers {
     bundler.contents.remaining should be (empty)
   }
 
+  it should "not be used if it's more expensive than retail" in {
+    val tomato = Item("Tomato", 1.0)
+    val twoTomatoBundle = Seq(Bundle(List(tomato, tomato), 3.0))
+    val cart = new Cart()
+    cart.add(tomato, tomato)
+    val bundler = new Bundler(twoTomatoBundle, cart)
+    bundler.contents.total should be (2.0)
+    bundler.contents.discount should be (0.0)
+    bundler.contents.bundles should be (empty)
+    bundler.contents.remaining should contain theSameElementsAs List(tomato, tomato)
+  }
+
   it should "find the cheapest bundle, which in this case is the six-tomatoe bundle" in {
     val tomato = Item("Tomato", 1.0)
-    val breadFreeTomatoBundle = Set(Bundle(List(bread, tomato), 3.0))
-    val sixTomatoBundle = Set(Bundle(List(tomato, tomato, tomato, tomato, tomato, tomato), 2.0))
+    val breadFreeTomatoBundle = Seq(Bundle(List(bread, tomato), 3.0))
+    val sixTomatoBundle = Seq(Bundle(List(tomato, tomato, tomato, tomato, tomato, tomato), 2.0))
     val cart = new Cart()
     cart.add(bread, tomato, tomato, tomato, tomato, tomato, tomato)
     val bundler = new Bundler(breadFreeTomatoBundle ++ sixTomatoBundle, cart)
     bundler.contents.total should be (5.0)
     bundler.contents.discount should be (4.0)
     bundler.contents.bundles should contain theSameElementsAs sixTomatoBundle
-    bundler.contents.remaining should contain only (bread)
+    bundler.contents.remaining should contain only bread
   }
 
 }
